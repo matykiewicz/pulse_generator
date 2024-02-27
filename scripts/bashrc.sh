45e3b97657ebf966771e4072bda950d04d7ce9f3
Entaroadun <pawel.matykiewicz@gmail.com>
2024-02-26 21:44:22 -0800

    Tweaks to pre-install

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -3,8 +3,10 @@
 #
 # sudo apt-get install libportaudio2 libasound-dev
 # sudo apt-get install python3-pip python3-venv
+# sudo apt-get install alsa alsa-utils
+# sudo apt install python3.9 python3.9-venv
 # python3 -m venv ~/.venv
-# ../.venv/bin/pip3 install poetry
+# ~/.venv/bin/pip3 install poetry
 sudo setfont /usr/share/consolefonts/Lat7-Terminus32x16.psf.gz
 amixer -c 0 sset 'PCM' 85%
 amixer -c 1 sset 'PCM' 85%
@@ -13,5 +15,6 @@ amixer -c 3 sset 'PCM' 85%
 amixer -c 4 sset 'PCM' 85%
 source ~/.venv/bin/activate
 cd ~/pulse_generator/
-../.venv/bin/poetry install
-../.venv/bin/poetry run python3 ./pulse_generator/cli.py -t 120 -s 16 -w 1 -a 1 -f 50
+rm -rf rm poetry.lock
+~/.venv/bin/poetry install
+~/.venv/bin/poetry run python3 ./pulse_generator/cli.py -t 120 -s 16 -w 1 -a 1 -f 50
--- scripts/deploy_code.sh
+++ scripts/deploy_code.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-DEST="pawelm@172.16.3.72"
+DEST="jetson@172.16.3.79"
 
 ssh "${DEST}"  "rm -rf ~/pulse_generator"
 scp -r ../pulse_generator "${DEST}":~/pulse_generator
