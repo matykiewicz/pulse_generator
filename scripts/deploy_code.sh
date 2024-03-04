060a641c69d27b9bc70a708d30e41720144c6d78
Entaroadun <pawel.matykiewicz@gmail.com>
2024-03-03 22:56:36 -0800

    Better documentation

--- scripts/deploy_code.sh
+++ scripts/deploy_code.sh
@@ -1,7 +1,9 @@
 # /bin/bash
 
-DEST="jetson@192.168.8.228"
+IP=${IP:-192.168.8.228}
+DEST="jetson@${IP}"
 
+echo "IP = $IP"
 ssh "${DEST}"  "rm -rf ~/pulse_generator"
 scp -r ../pulse_generator "${DEST}":~/pulse_generator
 
